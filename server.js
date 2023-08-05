800b3140eea9a3659a1951afec81b2e2e5297986
Amin23062004 <141436654+Amin23062004@users.noreply.github.com>
2023-08-05 20:05:02 +0300

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.use(express.json());
 
 // Middleware для обработки заголовков CORS
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://tez-tez-zakaz.onrender.com');
+    res.setHeader('Access-Control-Allow-Origin', '/your-endpoint');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
